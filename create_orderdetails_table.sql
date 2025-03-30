3c74c759d1a6f77688d97132f40e35cf8c456b9a
Michael Joseph <michael.ngala@strathmore.edu>
2025-03-30 17:07:33 +0300

    Update create_orderdetails_table.sql

--- create_orderdetails_table.sql
+++ create_orderdetails_table.sql
@@ -1,1 +1,25 @@
-SELECT * FROM orderdetails;
+-- Table: public.orderdetails
+
+-- DROP TABLE IF EXISTS public.orderdetails;
+
+CREATE TABLE IF NOT EXISTS public.orderdetails
+(
+    orderid character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    productid character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    quantity integer,
+    unitprice numeric(10,2),
+    CONSTRAINT orderdetails_pkey PRIMARY KEY (orderid, productid),
+    CONSTRAINT orderdetails_orderid_fkey FOREIGN KEY (orderid)
+        REFERENCES public.orders (orderid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT orderdetails_productid_fkey FOREIGN KEY (productid)
+        REFERENCES public.inventory (productid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.orderdetails
+    OWNER to postgres;
